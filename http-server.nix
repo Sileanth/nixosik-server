67625b2177a955100c2cefca7d946a40fbd3f2ff
sileanth <lukasz.magnuszewski@gmail.com>
2025-01-30 12:03:02 +0100

    setuping ssl

--- http-server.nix
+++ http-server.nix
@@ -3,14 +3,21 @@
    networking.firewall.allowedTCPPorts = [ 80 443];
 
 
+    security.acme.acceptTerms = true;
+    security.acme.defaults.email = "lukasz.magnuszewski@gmail.com";
+
+
+
    services.nginx = {
        enable = true;
        virtualHosts."sileanth.eu" = {
-locations."/" = {
-      return = "200 '<html><body>It works3</body></html>'";
-      extraConfig = ''
-        default_type text/html;
-      '';
+        forceSSL = false;
+        enableACME = true;
+        locations."/" = {
+            return = "200 '<html><body>It works3</body></html>'";
+            extraConfig = ''
+              default_type text/html;
+            '';
         };
      };
    };
