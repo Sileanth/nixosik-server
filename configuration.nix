14e7553cd1b835e2c8a95f355b272f5b3338b15e
sileanth <lukasz.magnuszewski@gmail.com>
2025-01-30 10:52:32 +0100

    nginx

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,10 @@
      };
    };
    networking.firewall.allowedTCPPorts = [ 22 ];
+
+
+
+   services.nginx.enable = true;
    
    # This value determines the NixOS release from which the default
    # settings for stateful data, like file locations and database versions
