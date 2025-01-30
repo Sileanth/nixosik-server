be69afa3c67088d7d50a95b95d2a71d776047488
sileanth <lukasz.magnuszewski@gmail.com>
2025-01-30 11:11:27 +0100

    listen nginx

--- http-server.nix
+++ http-server.nix
@@ -5,6 +5,10 @@
 
    services.nginx = {
        enable = true;
+  listen = [
+    { addr = "0.0.0.0"; port = 80; }
+    { addr = "[::]"; port = 80; }
+  ];
        virtualHosts."sileanth.eu" = {
           return = "200 '<html><body>It works</body></html>'";
       extraConfig = ''
