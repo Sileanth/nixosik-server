77f847a355035f7ee8498a3a18cf53a1c9af703b
sileanth <lukasz.magnuszewski@gmail.com>
2025-01-30 12:13:00 +0100

    blog nginx

--- http-server.nix
+++ http-server.nix
@@ -3,6 +3,7 @@
    networking.firewall.allowedTCPPorts = [ 80 443];
 
 
+    # lets encrypt ssl provider
     security.acme.acceptTerms = true;
     security.acme.defaults.email = "lukasz.magnuszewski@gmail.com";
 
@@ -10,15 +11,31 @@
 
    services.nginx = {
        enable = true;
-       virtualHosts."sileanth.eu" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-            return = "200 '<html><body>It works3</body></html>'";
-            extraConfig = ''
-              default_type text/html;
-            '';
-        };
+       virtualHosts = let
+        common = {
+
+          forceSSL = true;
+          enableACME = true;
+          };
+
+        in {
+          "blog.sileanth.eu" = common // {
+              locations."/" = {
+                  return = "200 '<html><body>Blog</body></html>'";
+                  extraConfig = ''
+                    default_type text/html;
+                  '';
+              };
+          };
+ 
+          "sileanth.eu" = common // {
+              locations."/" = {
+                  return = "200 '<html><body>It works3</body></html>'";
+                  extraConfig = ''
+                    default_type text/html;
+                  '';
+              };
+          };
      };
    };
   }
