ea9ae65400045b0beb56b00fde777cd244f487a1
sileanth <lukasz.magnuszewski@gmail.com>
2025-02-16 10:22:49 +0100

    hardenized headers

--- http-server.nix
+++ http-server.nix
@@ -24,27 +24,17 @@
         recommendedTlsSettings = true;
 
         appendHttpConfig = ''
-              # Add HSTS header with preloading to HTTPS requests.
-              # Adding this header to HTTP requests is discouraged
-              map $scheme $hsts_header {
-                  https   "max-age=31536000; includeSubdomains; preload";
-              }
-              add_header Strict-Transport-Security $hsts_header;
-
-              # Enable CSP for your services.
-              #add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always;
-
-              # Minimize information leaked to other domains
-              add_header 'Referrer-Policy' 'origin-when-cross-origin';
-
-              # Disable embedding as a frame
-              add_header X-Frame-Options DENY;
-
-              # Prevent injection of code in other mime types (XSS Attacks)
+              add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload";
+              add_header Content-Security-Policy "default-src 'self'; img-src * data:; style-src 'self' 'unsafe-inline'";
+              add_header X-XSS-Protection "0";
+#add_header X-XSS-Protection "1; mode=block"; # can potentaily be unsafe, attacker can disable some parts of js
+              add_header X-Frame-Options "SAMEORIGIN";
               add_header X-Content-Type-Options nosniff;
+              add_header Referrer-Policy "strict-origin";
+              add_header Permissions-Policy "geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=()";
 
-              # This might create errors
-              # proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict";
+
+
             '';
 
        virtualHosts = let
