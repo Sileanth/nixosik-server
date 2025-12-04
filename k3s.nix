5c55578d229ee00b171bf6815e5ca6dc41e93f15
sileanth <sileanth@sileanth.pl>
2025-12-04 10:13:49 +0100

    k3s

--- k3s.nix
+++ k3s.nix
@@ -2,14 +2,18 @@
   
    networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-    # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-    # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
   ];
   networking.firewall.allowedUDPPorts = [
-    # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+    8472 # k3s, flannel: required if using multi-node for inter-node networking
   ];
-  services.k3s.enable = true;
-  services.k3s.role = "server";
+  services.k3s = {
+    enable = true;
+    role = "server";
+    clusterInit = true;
+    tokenFile = "/secrets/k3s";
+  };
  
 
 
