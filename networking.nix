62626300cc84950aaa46f1beea284cddf9d0a5cb
Your Name <you@example.com>
2026-05-03 15:18:31 +0200

    .

--- networking.nix
+++ networking.nix
@@ -1,14 +1,14 @@
 { config, lib, pkgs, hostInfo, name, ... }:
 
 {
-  # networking.useDHCP = false;
-  # networking.useNetworkd = true;
-  #
+  networking.useDHCP = false;
+  networking.useNetworkd = true;
+
   networking.hostName = name;
-  # systemd.network.networks."10-wan" = {
-  #   matchConfig.Name = hostInfo.interface;
-  #   networkConfig.Address = [ "${hostInfo.private}/24" ];
-  #   networkConfig.Gateway = "10.0.0.1";
-  #   networkConfig.DNS = [ "169.254.169.254" ];
-  # };
+  systemd.network.networks."10-wan" = {
+    matchConfig.Name = hostInfo.interface;
+    networkConfig.Address = [ "${hostInfo.private}/24" ];
+    networkConfig.Gateway = "10.0.0.1";
+    networkConfig.DNS = [ "169.254.169.254" ];
+  };
 }
