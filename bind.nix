ba947d7f177590b6fb4cf210fda1d8bbf72f7b4e
Your Name <you@example.com>
2026-05-09 17:34:29 +0200

    close bind port, first need to disable resucive quries

--- bind.nix
+++ bind.nix
@@ -12,8 +12,8 @@ let
   isSlave  = name == "kotek" || name == "piesek";
 
 in {
-  networking.firewall.allowedTCPPorts = [ 53 ];
-  networking.firewall.allowedUDPPorts = [ 53 ];
+  # networking.firewall.allowedTCPPorts = [ 53 ];
+  # networking.firewall.allowedUDPPorts = [ 53 ];
   services.bind = {
     enable = true;
     
