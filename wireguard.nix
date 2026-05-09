4924c4fcead876ef007854e025462419df1b654a
Your Name <you@example.com>
2026-05-09 10:09:28 +0200

    wg

--- wireguard.nix
+++ wireguard.nix
@@ -2,10 +2,10 @@
 
 let
   isHub = name == "main";
-  
+
   # Filter out the current host from the peers list
   peerHosts = lib.filterAttrs (n: v: n != name) hosts;
-  
+
   # Helper to create a peer config
   mkPeer = peerName: peerInfo: {
     PublicKey = peerInfo.wgPubKey;
@@ -18,6 +18,13 @@ let
 
 in
 {
+  environment.systemPackages = [ pkgs.wireguard-tools ];
+
+  systemd.tmpfiles.rules = [
+    "d /etc/wireguard 0750 root systemd-network - -"
+    "z /etc/wireguard/private.key 0640 root systemd-network - -"
+  ];
+
   systemd.network.netdevs."50-wg0" = {
     netdevConfig = {
       Kind = "wireguard";
@@ -25,11 +32,13 @@ in
       Description = "WireGuard VPN";
     };
     wireguardConfig = {
-      ListenPort = if isHub then 51820 else null;
       PrivateKeyFile = "/etc/wireguard/private.key";
+      RouteTable = "main";
+    } // lib.optionalAttrs isHub {
+      ListenPort = 51820;
     };
     # Hub connects to everyone; Nodes only connect to Hub
-    wireguardPeers = if isHub 
+    wireguardPeers = if isHub
       then lib.mapAttrsToList mkPeer peerHosts
       else [ (mkPeer "main" hosts.main) ];
   };
@@ -38,19 +47,19 @@ in
     matchConfig.Name = "wg0";
     address = [ "${hostInfo.vpnIp}/24" ];
     networkConfig = lib.mkIf isHub {
-        IPv4Forwarding = true;
-        IPv6Forwarding = true;
+      DHCP = "no";
+      LinkLocalAddressing = "no";
+      IPv4Forwarding = true;
+      IPv6Forwarding = true;
     };
-
   };
 
   networking.nat = lib.mkIf isHub {
-enable = true;
+    enable = true;
     enableIPv6 = true;
-    externalInterface = "ens6";
+    externalInterface = hostInfo.interface;
     internalInterfaces = [ "wg0" ];
-
   };
 
-  networking.firewall.allowedUDPPorts = [ 51820 ];
+  networking.firewall.allowedUDPPorts = lib.mkIf isHub [ 51820 ];
 }
