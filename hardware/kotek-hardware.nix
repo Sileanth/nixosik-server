61b2a55bd79a8307474504d76ad8855cb16d3f08
Your Name <you@example.com>
2026-05-09 12:03:23 +0200

    serial connection for troubleshooting

--- hardware/kotek-hardware.nix
+++ hardware/kotek-hardware.nix
@@ -15,5 +15,10 @@
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
-  
+   boot.kernelParams = [ "console=ttyS0,115200n8" ];
+ boot.loader.grub.extraConfig = "
+   serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
+   terminal_input serial
+   terminal_output serial
+ ";
 }
