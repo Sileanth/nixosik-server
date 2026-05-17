0ef7ad57effb0ad3e98d206ba5a2f78140662292
Your Name <you@example.com>
2026-05-17 14:39:52 +0200

    bind2

--- bind.nix
+++ bind.nix
@@ -10,24 +10,58 @@ let
 
   isMaster = name == "main";
   isSlave  = name == "kotek" || name == "piesek";
+  trustedSlaves = [ "${kotekIp}/32" "${piesekIp}/32" ];
+  masterAcl     = [ "${mainIp}/32" ];
 
 in {
   # networking.firewall.allowedTCPPorts = [ 53 ];
   # networking.firewall.allowedUDPPorts = [ 53 ];
   services.bind = {
     enable = true;
-    
+    extraConfig = ''
+
+      acl "slaves"  { ${lib.concatStringsSep "; " trustedSlaves}; };
+      acl "masters" { ${lib.concatStringsSep "; " masterAcl};     };
+    '';
+   extraOptions = ''
+    version "none";
+    recursion no;
+    max-udp-size 512;
+    tcp-clients 50;
+    tcp-idle-timeout 5000;    # 5 s in milliseconds
+    deny-answer-addresses { any; } except-from { "${domain}"; };
+      #    - "responses-per-second": hard cap on identical answers
+      #    - "all-per-second": cap on ALL responses to one source (anti-flood)
+      #    - "slip 2": every 2nd excess query gets a TRUNCATED answer,
+      #      nudging legit clients to retry over TCP (amplification is UDP-only)
+      #    - "window 5": rolling 5-second accounting window
+      rate-limit {
+        responses-per-second 5;
+        referrals-per-second  2;
+        nodata-per-second     5;
+        errors-per-second     5;
+        all-per-second       20;
+        slip                  2;
+        window                5;
+        log-only             no;
+      };
+   '';
 
     zones = lib.mkIf (isMaster || isSlave) {
       "${domain}" = if isMaster then {
         master = true;
         slaves = [ kotekIp piesekIp ];
+        extraConfig = ''
+          allow-transfer  { slaves; };
+          allow-notify    { slaves; };
+          notify          yes;
+          also-notify     { ${kotekIp}; ${piesekIp}; };
+        '';
         file = pkgs.writeText "${domain}.zone" ''
-          ; ULTRA-FAST TEST SETUP
           $TTL 30      ; Default TTL (30 seconds)
           
           @       IN      SOA     ns0.${domain}. admin.${domain}. (
-                          2026050301 ; serial
+                          2026050302 ; serial
                           60         ; refresh (1 min)   - PROD: 7200 (2h)
                           30         ; retry (30 sec)    - PROD: 3600 (1h)
                           120        ; expire (2 mins)   - PROD: 1209600 (2w)
@@ -53,6 +87,10 @@ in {
         master = false;
         masters = [ mainIp ];
         file = "/var/lib/bind/${domain}.zone";
+        extraConfig = ''
+          allow-transfer  { none; };
+          allow-notify    { masters; };
+        '';
       };
     };
   };
