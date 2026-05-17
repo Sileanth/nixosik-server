1a07fe672adf35b8578046752998c7374a0bd393
Your Name <you@example.com>
2026-05-17 14:25:25 +0200

    bindbind

--- bind.nix
+++ bind.nix
@@ -3,7 +3,7 @@
 let
   hosts = import ./hosts.nix;
   domain = "sileanth.pl";
-  
+
   mainIp   = hosts.main.public;
   kotekIp  = hosts.kotek.public;
   piesekIp = hosts.piesek.public;
@@ -12,48 +12,75 @@ let
   isSlave  = name == "kotek" || name == "piesek";
 
 in {
-  # networking.firewall.allowedTCPPorts = [ 53 ];
-  # networking.firewall.allowedUDPPorts = [ 53 ];
+
+  networking.firewall.allowedTCPPorts = [ 53 ];
+  networking.firewall.allowedUDPPorts = [ 53 ];
+
+
   services.bind = {
     enable = true;
-    
+
+    extraOptions = ''
+      recursion no;
+      allow-query { any; };
+
+      version "Not Disclosed";
+      server-id none;
+
+      rate-limit {
+        responses-per-second 10;
+        errors-per-second 5;
+        window 5;
+        log-only no;
+      };
+
+      deny-answer-aliases { any; };
+    '';
 
     zones = lib.mkIf (isMaster || isSlave) {
       "${domain}" = if isMaster then {
         master = true;
         slaves = [ kotekIp piesekIp ];
+
+        allowTransfer = [ kotekIp piesekIp ];
+
+        extraConfig = ''
+          also-notify { ${kotekIp}; ${piesekIp}; };
+          notify yes;
+        '';
+
         file = pkgs.writeText "${domain}.zone" ''
-          ; ULTRA-FAST TEST SETUP
-          $TTL 30      ; Default TTL (30 seconds)
-          
+          $TTL 3600    ; 1 Hour default caching limit
+
           @       IN      SOA     ns0.${domain}. admin.${domain}. (
-                          2026050301 ; serial
+                          2026051801 ; Serial (YYYYMMDDNN style updated for May 2026)
                           60         ; refresh (1 min)   - PROD: 7200 (2h)
                           30         ; retry (30 sec)    - PROD: 3600 (1h)
                           120        ; expire (2 mins)   - PROD: 1209600 (2w)
                           30         ; minimum (30 sec)  - PROD: 3600 (1h)
           )
-          
-          ; Name Servers
+
           @       IN      NS      ns0.${domain}.
           @       IN      NS      ns1.${domain}.
           @       IN      NS      ns2.${domain}.
-          
-          ; Glue Records / A Records for NS
+
           ns0     IN      A       ${mainIp}
           ns1     IN      A       ${kotekIp}
           ns2     IN      A       ${piesekIp}
-          
-          ; Main traffic records
+
           @       IN      A       ${mainIp}
           *       IN      A       ${mainIp}
-
         '';
       } else {
         master = false;
         masters = [ mainIp ];
-        file = "/var/lib/bind/${domain}.zone";
+
+        allowTransfer = [ "none" ];
+
+        file = "/var/lib/bind/db.${domain}";
       };
     };
   };
+
+
 }
